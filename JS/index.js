0754f3c2bb63f795da93a0be7b28b9299b42c0e4
etane27 <etane.babel@gmail.com>
2024-04-14 00:58:43 +0200

    particles ajoutés sur login et register

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+let connexion = document.getElementById("connect_button");
+connexion.onclick = function () {
+  location.href = "../html/login.html";
+};
+
+let inscription = document.getElementById("start_button");
+inscription.onclick = function () {
+  location.href = "../html/register.html";
+};
